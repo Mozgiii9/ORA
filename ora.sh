bd70ed4e0fa30de8bb853fb1627218aa517da3e7
Nikita Mozgin <74683169+Mozgiii9@users.noreply.github.com>
2024-09-16 18:09:19 +0300

    Update ora.sh

--- ora.sh
+++ ora.sh
@@ -32,7 +32,10 @@ then
     sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt install docker-ce docker-ce-cli containerd.io -y    
+    sudo apt install docker-ce docker-ce-cli containerd.io -y
+    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    docker-compose --version
 else
     show "Docker уже установлен."
 fi
